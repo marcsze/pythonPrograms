b2fdc42f73a83948debdb4049f3b715854005579
Marc Sze <marcasze@gmail.com>
2016-05-22 12:20:16 -0400

    add column label functionality

--- compareDoc.py
+++ compareDoc.py
@@ -7,8 +7,13 @@
 # sequencingID, GroupID, file found in.  
 # An example of how to use this program is below:
 
-## python compareDoc.py samplesToMatch.txt "FD" sample.txt output.txt
+## python compareDoc.py samplesToMatch.txt "FD" sample.txt -t output.txt
 
+## samplesToMatch.txt is the file containing samples you want to check
+## "FD" can be any term but this is the pattern you are selecting out for in samplesToMatch.txt
+## sample.txt is the file you are searching for samples in
+## -t moniker "t" if this is the first file you have searched
+## output.txt is the name of the ouput file
 
 # Read in necessary programs and functions
 import sys
@@ -21,8 +26,9 @@ def commandLine():
 	textToConvert = commands[1]
 	pattern = commands[2]
 	mappingFile = commands[3]
-	outputfile = commands[4]
-	return textToConvert, pattern, mappingFile, outputfile
+	start = commands[4]
+	outputfile = commands[5]
+	return textToConvert, pattern, mappingFile, start, outputfile
 
 # Read in test data as a dictionary to modify file as needed
 def readTestFile(mappingFile, pattern):
@@ -52,9 +58,12 @@ def compareData(dataList, testDict):
 	
 	
 # This function will output a tab-delimited text file of the samples
-def saveFindings(YESMatchDict, mappingFile, outputfile):
+def saveFindings(YESMatchDict, mappingFile, start, outputfile):
 	#change to 'a' for appending to file rather than write (w)
 	outfile = open(outputfile, 'a')
+	if "t" in start:
+		print("{0}\t{1}\t{2}".format("SequencingID", "SampleID", "MapFile"), 
+		end='\n', file=outfile)
 	for i in YESMatchDict:
 		sample = YESMatchDict[i]
 		print("{0}\t{1}\t{2}".format(sample, i, mappingFile), 
@@ -63,11 +72,12 @@ def saveFindings(YESMatchDict, mappingFile, outputfile):
 
 # Run the overall program
 def main():
-	textToConvert, pattern, mappingFile, outputfile  = commandLine()
+	textToConvert, pattern, mappingFile, start, outputfile  = commandLine()
 	dataList = readData(textToConvert)
 	goodData = identifySamples(dataList, pattern)
 	testDict = readTestFile(mappingFile, pattern)
 	YESMatchDict, noMatchList = compareData(dataList, testDict)
-	saveFindings(YESMatchDict, mappingFile, outputfile)
+	saveFindings(YESMatchDict, mappingFile, start, outputfile)
+		
 
 if __name__ == '__main__': main()
